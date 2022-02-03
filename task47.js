b5c41c300e1cc3b9c3968fc661c4b3235618f30a
Svemiliva <mshtatkich@gmail.com>
2022-02-03 15:20:42 +0100

    datum

---
+++ task47.js
@@ -0,0 +1,13 @@
+
+let datumKaoText = "2022-02-03";
+let datum = new Date(datumKaoText);
+
+console.log(datum.getFullYear());
+console.log(datum.getMonth());
+console.log(datum.getDay());
+console.log(datum.getHours());
+
+
+let datumD = new Date();
+console.log(datumD.getFullYear());
+// unix epoha 1.1.1970.
